7b41d74180417eb9a6d1c667fe834ffdf0e03b73
Ferdinand Malcher <mail@ferdinand-malcher.de>
2019-07-03 11:37:01 +0200

    Unidirektionaler Data Flow: Book und Dashboard mit Events und Service

--- book-rating/src/app/books/dashboard/dashboard.component.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.ts
@@ -11,7 +11,7 @@ export class DashboardComponent implements OnInit {
 
   books: Book[];
 
-  constructor() {}
+  constructor(private rs: BookRatingService) {}
 
   ngOnInit() {
     this.books = [
@@ -33,5 +33,22 @@ export class DashboardComponent implements OnInit {
   trackBook(index: number, book: Book) {
     return book.isbn;
   }
+
+  rateUp(book: Book) {
+    const ratedBook = this.rs.rateUp(book);
+    this.updateSortList(ratedBook);
+  }
+
+  rateDown(book: Book) {
+    const ratedBook = this.rs.rateDown(book);
+    this.updateSortList(ratedBook);
+  }
+
+  updateSortList(ratedBook: Book) {
+    this.books = this.books
+      .map(b => b.isbn === ratedBook.isbn ? ratedBook : b)
+      .sort((a, b) => b.rating - a.rating);
+  }
+
 
 }
