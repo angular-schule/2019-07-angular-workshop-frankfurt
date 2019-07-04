86a1bad3261a9f68e40db0dc2857868f651199df
Ferdinand Malcher <mail@ferdinand-malcher.de>
2019-07-04 11:09:20 +0200

    BookDetails mit HTTP – Achtung: Doppeltes subscribe!

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { BookStoreService } from '../shared/book-store.service';
+import { Book } from '../shared/book';
 
 @Component({
   selector: 'br-book-details',
@@ -9,16 +11,20 @@ import { ActivatedRoute } from '@angular/router';
 export class BookDetailsComponent implements OnInit {
 
   isbn: string;
+  book: Book;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private bs: BookStoreService) { }
 
   ngOnInit() {
     // Synchroner Weg
     // this.isbn = this.route.snapshot.paramMap.get('isbn');
 
-    // Asynchroner Weg
+    // TODO: doppeltes subscribe vermeiden (!!!)
     this.route.paramMap.subscribe(params => {
-      this.isbn = params.get('isbn');
+      const isbn = params.get('isbn');
+      this.bs.getSingle(isbn).subscribe(book => {
+        this.book = book;
+      });
     });
   }
 
